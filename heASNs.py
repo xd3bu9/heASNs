0fe1ced4bf13899c8daf97182a29144a05674b6b
xd3bu9 <000macharia@gmail.com>
2024-08-22 20:53:09 +0300

    support json output

--- heASNs.py
+++ heASNs.py
@@ -5,23 +5,28 @@ from bs4 import BeautifulSoup
 import os
 import re
 import requests
+import json
+import sys
 
 requests.packages.urllib3.disable_warnings(
     requests.packages.urllib3.exceptions.InsecureRequestWarning
 )
 
+resultlist = []
 ranges_pattern = re.compile(r"\b(?:\d{1,3}\.){3}\d{1,3}/\d{1,2}\b")
 asn_pattern = re.compile(r"\bAS\d{5}\b")
 
-
 def get_args():
     parser = ArgumentParser()
-    parser.add_argument("-i", help="organization name or input file.")
+    parser.add_argument("-i", metavar="INPUT", help="organization name or input file.")
     return parser.parse_args()
 
 
-def req(domain):
-    parameters = {"search[search]": domain.strip(), "commit": "Search"}
+def req(org):
+    parameters = {"search[search]": org.strip(), "commit": "Search"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+    }
     x = requests.get("https://bgp.he.net/search", params=parameters)
     html = BeautifulSoup(x.text, "html.parser")
     table = html.find("table", attrs={"class": "w100p"})
@@ -29,19 +34,11 @@ def req(domain):
     return atags
 
 
-def print_output(lst, line):
-    if len(lst) < 1:
-        return
-    print("----- " + line.strip() + " -----")
-    for item in lst:
-        print(item)
-
-
-def parse(links, line):
+def parse(links, org):
     ipRanges = list(set(re.findall(ranges_pattern, str(links))))
     asns = list(set(re.findall(asn_pattern, str(links))))
-    print_output(asns, line=line)
-    print_output(ipRanges, line=line)
+    item = {org: {"asns": asns, "ranges": ipRanges}}
+    resultlist.append(item)
 
 
 def file_exists(file):
@@ -52,14 +49,19 @@ def bgp(userInput):
     if file_exists(file=userInput):
         with open(userInput.strip(), "r") as file:
             for line in file:
-                parse(req(line), line=line)
+                parse(req(line), org=line)
     else:
-        parse(req(userInput), userInput)
+        parse(req(userInput), org=userInput)
 
 
 def main():
     args = get_args()
+    if args.i == None:
+        print("-i is a required argument.")
+        exit(1)
     bgp(userInput=args.i)
+    output = {"results": resultlist}
+    print(json.dumps(output))
 
 
 if __name__ == "__main__":
